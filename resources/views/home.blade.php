0127965b8e64dace3ece16ea7c2f67c02d1046aa
lindabettini <linda.bettini@hotmail.it>
2022-03-15 15:45:08 +0100

    add ul pages links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,16 @@
 </head>
 
 <body>
-
+  <header>
+    <div class="top-header"></div>
+    <nav>
+      <ul>
+        @foreach ($pages as $title=>$route)
+        <li><a href="{{route($route)}}">{{$title}}</a></li>
+        @endforeach
+      </ul>
+    </nav>
+  </header>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,28 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home');
+
+    $data = [
+        'pages' => [
+            'Comics' => 'home',
+            'Characters' => 'characters',
+            'Games' => 'games',
+            'News' => 'news',
+        ],
+    ];
+
+    return view('home', $data);
 })->name('home');
+
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name('characters');
+
+Route::get('/games', function () {
+    return view('games');
+})->name('games');
+
+Route::get('/news', function () {
+    return view('news');
+})->name('news');
