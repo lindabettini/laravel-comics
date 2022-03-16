cbfb1e5d41a0c296a8e1bbbf9ed53453b1f4cf40
lindabettini <linda.bettini@hotmail.it>
2022-03-16 15:16:02 +0100

    add style

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,13 +5,26 @@ $links = config('main_menu');
 
 
 <header class="container">
-  <div id="top-header">ciaoooooo</div>
-  <nav id="bottom-header">
-    <ul>
-      <li><a href="{{ url('/') }}">Comics</a></li>
-      <li><a href="{{route('characters')}}">Character</a></li>
-      <li><a href="{{route('games')}}">Games</a></li>
-      <li><a href="{{route('news')}}">News</a></li>
-    </ul>
-  </nav>
+  <div class="row">
+    <div class="col">
+      <div id="top-header" class="d-flex content-end">
+        <div class="p-2">DC POWER VISA</div>
+        <div class="p-2">ADDITIONAL DC SITES</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col">
+      <nav id="bottom-header">
+        <ul>
+          <li><a href="{{ url('/') }}">Comics</a></li>
+          <li><a href="{{route('characters')}}">Character</a></li>
+          <li><a href="{{route('games')}}">Games</a></li>
+          <li><a href="{{route('news')}}">News</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+
 </header>
