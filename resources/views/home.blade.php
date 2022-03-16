2b490a7733a6e7287474da0e1304a1d860ec6aae
lindabettini <linda.bettini@hotmail.it>
2022-03-16 16:43:03 +0100

    add comics datas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layout.main')
 
+@php
+$comics = config('comics_data');
+@endphp
+
 @section('main')
-<h1>CAIOOOO</h1>
+<div class="container">
+  <div class="row">
+    @foreach ($comics as $comic)
+    <div class="col">
+      <div class="comic-card">
+        <img src="{{$comic['thumb']}}" alt="">
+      </div>
+    </div>
+    @endforeach
+  </div>
+</div>
 @stop
