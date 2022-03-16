4c23556739c418565ad6057398b7bdef86eb28f8
lindabettini <linda.bettini@hotmail.it>
2022-03-16 23:12:09 +0100

    footer complete

--- resources/views/includes/footbottom.blade.php
+++ resources/views/includes/footbottom.blade.php
@@ -1,15 +1,15 @@
-<!-- <div id="bottom-footer">
+<div id="bottom-footer">
   <div class="container">
     <div id="sing-up" class="col-6">
       <button class="clickable">SING-UP NOW!</button>
     </div>
-   <div id="socials" class="col-6">
-      <h4>FOLLOW US</h4>
-      <a href="#"><img src="../assets/img/footer-facebook.png" alt="Facebook" /></a>
-      <a href="#"><img src="../assets/img/footer-twitter.png" alt="Tiwitter" /></a>
-      <a href="#"><img src="../assets/img/footer-youtube.png" alt="Facebook" /></a>
-      <a href="#"><img src="../assets/img/footer-pinterest.png" alt="Facebook" /></a>
-      <a href="#"><img src="../assets/img/footer-periscope.png" alt="Facebook" /></a>
-    </div> 
+    <div id="socials" class="col-6">
+      <h3>FOLLOW US</h3>
+      <a href="#"><img src="images/footer-facebook.png" alt="Facebook" /></a>
+      <a href="#"><img src="images/footer-twitter.png" alt="Tiwitter" /></a>
+      <a href="#"><img src="images/footer-youtube.png" alt="Facebook" /></a>
+      <a href="#"><img src="images/footer-pinterest.png" alt="Facebook" /></a>
+      <a href="#"><img src="images/footer-periscope.png" alt="Facebook" /></a>
+    </div>
   </div>
-</div> -->
+</div>
